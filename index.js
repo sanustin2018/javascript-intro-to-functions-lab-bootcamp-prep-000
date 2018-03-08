dfd215c8ef75d769231ea569ab5265c51c93b691
sperrysz39@gmail.com <sperrysz39@gmail.com>
2018-03-08 18:35:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function sayHiToGrandma(string) {
   return ("I can\'t hear you!")
 if (string.toUpperCase()== string)
 return ("YES INDEED!")
-if (string.toMixCase()== )
+if (string.toMixCase()== 'I love you, Grandma.')
 }
